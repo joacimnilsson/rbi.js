5cf4f656d108b0a6f81599614f99c5a0c0111efa
joacimnilsson <joacim@joacimnilsson.se>
2016-03-02 18:39:27 +0100

    Reduce code

--- rbi.js
+++ rbi.js
@@ -14,101 +14,37 @@
     Repo: https://github.com/joacimnilsson/rbi.js
  */
 function rbi() {
-	
-	function erbi() {
-		var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
-		    imgs = document.querySelectorAll('.rbi');
 
-		[].forEach.call(imgs, function(img) {
-			var s = 		img.style,
-			    small = 		img.getAttribute('data-rbi-small'),
-			    medium = 		img.getAttribute('data-rbi-medium'),
-			    large = 		img.getAttribute('data-rbi-large'),
-			    xlarge = 		img.getAttribute('data-rbi-xlarge'),
-			    xxlarge = 		img.getAttribute('data-rbi-xxlarge'),
-			    size = 		img.getAttribute('data-rbi-size'),
-			    position = 		img.getAttribute('data-rbi-position'),
-			    background = 	"";
-			
-			if(w <= 640) {
-				if(small != "" && small != null){
-					background = small;
-				} else if(medium != "" && medium != null) {
-					background = medium;
-				} else if(large != "" && large != null) {
-					background = large;
-				} else if(xlarge != "" && xlarge != null) {
-					background = xlarge;
-				} else if(xxlarge != "" && xxlarge != null) {
-					background = xxlarge;
-				}
-			}
-			
-			if(w >= 641 && w <= 1024) {
-				if(medium != "" && medium != null) {
-					background = medium;
-				} else if(large != "" && large != null) {
-					background = large;
-				} else if(xlarge != "" && xlarge != null) {
-					background = xlarge;
-				} else if(xxlarge != "" && xxlarge != null) {
-					background = xxlarge;
-				} else if(small != "" && small != null){
-					background = small;
-				}
-			}
-			
-			if(w >= 1025 && w <= 1440) {
-				if(large != "" && large != null) {
-					background = large;
-				} else if(xlarge != "" && xlarge != null) {
-					background = xlarge;
-				} else if(xxlarge != "" && xxlarge != null) {
-					background = xxlarge;
-				} else if(medium != "" && medium != null) {
-					background = medium;
-				} else if(small != "" && small != null) {
-					background = small;
-				}
-			}
-			
-			if(w >= 1441 && w <= 1920) {
-				if(xlarge != "" && xlarge != null) {
-					background = xlarge;
-				} else if(xxlarge != "" && xxlarge != null) {
-					background = xxlarge;
-				} else if(large != "" && large != null) {
-					background = large;
-				} else if(medium != "" && medium != null) {
-					background = medium;
-				} else if(small != "" && small != null) {
-					background = small;
-				}
-			}
-			
-			if(w >= 1921) {
-				if(xxlarge != "" && xxlarge != null) {
-					background = xxlarge;
-				} else if(xlarge != "" && xlarge != null) {
-					background = xlarge;
-				} else if(large != "" && large != null) {
-					background = large;
-				} else if(medium != "" && medium != null) {
-					background = medium;
-				} else if(small != "" && small != null) {
-					background = small;
-				}
-			}
-			
-			s.backgroundImage = 'url("' + background + '")';
-			s.backgroundSize = size;
-			s.backgroundPosition = position;
-		});
-	}
-	
-	erbi();
+    function rbiUpdate() {
+        var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+            images = document.querySelectorAll('.rbi');
 
-	window.onresize = function(event) {
-		erbi();
-	};
+        [].forEach.call(images, itterate);
+
+        function itterate(img) {
+            var small = 	img.getAttribute('data-rbi-small'),
+            	medium = 	img.getAttribute('data-rbi-medium'),
+           	large = 	img.getAttribute('data-rbi-large'),
+            	xlarge = 	img.getAttribute('data-rbi-xlarge'),
+            	xxlarge = 	img.getAttribute('data-rbi-xxlarge'),
+            	size = 		img.getAttribute('data-rbi-size'),
+            	position = 	img.getAttribute('data-rbi-position'),
+            	style = 	img.style,
+            	background =
+                (w > 1920 	&& 	(xxlarge 	|| xlarge 	|| large 	|| medium 	|| small)) 	||
+                (w > 1440 	&& 	(xlarge 	|| xxlarge 	|| large 	|| medium 	|| small)) 	||
+                (w > 1024 	&& 	(large 		|| xlarge 	|| xxlarge 	|| medium 	|| small)) 	||
+                (w > 640 	&& 	(medium 	|| large 	|| xlarge 	|| xxlarge 	|| small)) 	||
+                (w > 0 		&& 	(small 		|| medium 	|| large 	|| xlarge 	|| xxlarge))
+
+            style.backgroundImage = 'url("' + background + '")';
+            style.backgroundSize = size;
+            style.backgroundPosition = position;
+        };
+    }
+
+    rbiUpdate();
+
+    window.removeEventListener('resize', rbiUpdate)
+    window.addEventListener('resize', rbiUpdate)
 }
